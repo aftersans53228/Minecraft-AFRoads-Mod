d47f8b8607e8afefc42e81546fd23dc13557aa01
aftersans53228 <aftersans53228@outlook.com>
2025-12-27 22:16:26 +0800

    1.19-1.19.2 brach
    (cherry picked from commit 0f962dc3fbfcddb83c64b18117dfbf2789420adf)

--- src/main/java/io/github/aftersans53228/aft_fabroads/render/RoadLightEntityRender.java
+++ src/main/java/io/github/aftersans53228/aft_fabroads/render/RoadLightEntityRender.java
@@ -22,8 +22,8 @@ import static net.minecraft.util.math.Direction.*;
 @Environment(EnvType.CLIENT)
 public class RoadLightEntityRender implements BlockEntityRenderer<RoadLightEntity> {
     //获得物品stack
-    private static final ItemStack RoadLightBulbCold = new ItemStack(AFRoadsItemRegistry.RoadLightBulbCold, 1);
-    private static final ItemStack RoadLightBulbWarm = new ItemStack(AFRoadsItemRegistry.RoadLightBulbWarm, 1);
+    private static final ItemStack ROAD_LIGHT_BULB_COLD = new ItemStack(AFRoadsItemRegistry.RoadLightBulbCold, 1);
+    private static final ItemStack ROAD_LIGHT_BULB_WARM = new ItemStack(AFRoadsItemRegistry.RoadLightBulbWarm, 1);
 
     public RoadLightEntityRender(BlockEntityRendererFactory.Context ctx) {}
     @Override
@@ -36,30 +36,25 @@ public class RoadLightEntityRender implements BlockEntityRenderer<RoadLightEntit
         if (blockEntity.getCachedState().get(Properties.HORIZONTAL_FACING)==SOUTH){
             matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(180));
         }
+        else if (blockEntity.getCachedState().get(Properties.HORIZONTAL_FACING)==NORTH){
+            matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(0));
+        }
+        else if (blockEntity.getCachedState().get(Properties.HORIZONTAL_FACING)==EAST){
+            matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(270));
+        }
+        else if (blockEntity.getCachedState().get(Properties.HORIZONTAL_FACING)==WEST){
+            matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(90));
+        }
         else{
-            if (blockEntity.getCachedState().get(Properties.HORIZONTAL_FACING)==NORTH){
-                matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(0));
-            }
-            else{
-                if (blockEntity.getCachedState().get(Properties.HORIZONTAL_FACING)==EAST){
-                    matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(270));
-                }
-                else{
-                    if (blockEntity.getCachedState().get(Properties.HORIZONTAL_FACING)==WEST){
-                        matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(90));
-                    }
-                    else{
-                        AFRoads.LOGGER.info("Unexpected road light orientation state.");
-                        matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(0));
-                    }
-                }
-            }
-        }//选择渲染类型
+            AFRoads.LOGGER.info("Unexpected road light orientation state.");
+            matrices.multiply(Vec3f.POSITIVE_Y.getDegreesQuaternion(0));
+        }
+        //选择渲染类型
             if (blockEntity.getCachedState().get(RoadLight.LightType)==0){
-                MinecraftClient.getInstance().getItemRenderer().renderItem(RoadLightBulbCold, ModelTransformation.Mode.GROUND, 15728880, OverlayTexture.DEFAULT_UV, matrices, vertexConsumers, 0);
+                MinecraftClient.getInstance().getItemRenderer().renderItem(ROAD_LIGHT_BULB_COLD, ModelTransformation.Mode.GROUND, 15728880, OverlayTexture.DEFAULT_UV, matrices, vertexConsumers, 0);
             }
             else{
-                MinecraftClient.getInstance().getItemRenderer().renderItem(RoadLightBulbWarm, ModelTransformation.Mode.GROUND, 15728880, OverlayTexture.DEFAULT_UV, matrices, vertexConsumers, 0);
+                MinecraftClient.getInstance().getItemRenderer().renderItem(ROAD_LIGHT_BULB_WARM, ModelTransformation.Mode.GROUND, 15728880, OverlayTexture.DEFAULT_UV, matrices, vertexConsumers, 0);
             }
 
         //GL拜拜了您内
