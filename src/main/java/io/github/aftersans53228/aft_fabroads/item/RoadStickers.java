e0326f30cef4695214a2a3be1835aebe5dfc9c5c
aftersans53228 <aftersans53228@outlook.com>
2026-07-15 19:59:59 +0800

    change dir

--- src/main/java/io/github/aftersans53228/aft_fabroads/item/RoadStickers.java
+++ src/main/java/io/github/aftersans53228/aft_fabroads/item/RoadStickers.java
@@ -16,16 +16,43 @@ public class RoadStickers {
             .icon(() -> new ItemStack(ArrowForwardItem))
             .displayName(Text.translatable("itemGroup.aft_fabroads.road_stickers"))
             .entries(((context, entries) -> {
-                entries.add(new ItemStack(RoadBlockItem));
-                entries.add(new ItemStack(RoadBlockConcreteItem));
-                entries.add(new ItemStack(ManholeCoverItem));
-                entries.add(new ItemStack(ManholeCoverConcreteItem));
-                entries.add(new ItemStack(RoadSeamsBlockItem));
-                entries.add(new ItemStack(RoadSeamsBlockConcreteItem));
-                entries.add(new ItemStack(ConcreteSlabItem));
-                entries.add(new ItemStack(ConcreteStairsItem));
-                entries.add(new ItemStack(ConcreteStairsSmoothItem));
-                entries.add(new ItemStack(ConcreteColumnsCornerItem));
+                entries.add(new ItemStack(LineStraightItem));
+                entries.add(new ItemStack(LineCornerItem));
+                entries.add(new ItemStack(LineTshapedItem));
+                entries.add(new ItemStack(LineCrossItem));
+                entries.add(new ItemStack(LineDiagonalItem));
+                entries.add(new ItemStack(LineLeftBendItem));
+                entries.add(new ItemStack(LineRightBendItem));
+                entries.add(new ItemStack(LineForkLeftItem));
+                entries.add(new ItemStack(LineForkRightItem));
+                entries.add(new ItemStack(LineStraightThickItem));
+                entries.add(new ItemStack(LineStraightDuoLineItem));
+                entries.add(new ItemStack(LineStraightDuoThickItem));
+                entries.add(new ItemStack(LineStraightDuoThickDashedItem));
+                entries.add(new ItemStack(LineDecelerateNoLineItem));
+                entries.add(new ItemStack(LineDecelerateNoLineFlipItem));
+                entries.add(new ItemStack(LineDecelerateWithLineItem));
+                entries.add(new ItemStack(LineDecelerateWithLineFlipItem));
+                entries.add(new ItemStack(LineDecelerateDoubleWLItem));
+                entries.add(new ItemStack(LineDecelerateDoubleNLItem));
+                entries.add(new ItemStack(LineReversibleLanesItem));
+                entries.add(new ItemStack(LineReversibleLanesFlipItem));
+                entries.add(new ItemStack(LineReversibleLanesDoubleItem));
+
+                entries.add(new ItemStack(ArrowForwardItem));
+                entries.add(new ItemStack(ArrowLeftItem));
+                entries.add(new ItemStack(ArrowRightItem));
+                entries.add(new ItemStack(ArrowForwardLeftItem));
+                entries.add(new ItemStack(ArrowForwardRightItem));
+                entries.add(new ItemStack(ArrowLeftRightItem));
+                entries.add(new ItemStack(ArrowBackItem));
+                entries.add(new ItemStack(ArrowBackLeftItem));
+                entries.add(new ItemStack(ArrowBackForwardItem));
+                entries.add(new ItemStack(ArrowConfluenceLeftItem));
+                entries.add(new ItemStack(ArrowConfluenceRightItem));
+
+                entries.add(new ItemStack(IconDecelerateStickerItem));
+                entries.add(new ItemStack(IconStopStickerItem));
             }))
             .build();
 }
